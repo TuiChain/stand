50f6c25c6cb015660937f76271357a936665df84
PedroFerr101 <pferreira59@gmail.com>
2021-02-01 06:02:06 +0000

    Add favicon

--- src/html.js
+++ src/html.js
@@ -16,6 +16,15 @@ export default function HTML({
   return (
     <html {...htmlAttributes}>
       <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="src/assets/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="src/assets/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="src/assets/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="src/assets/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="src/assets/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="src/assets/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="msapplication-config" content="src/assets/favicon/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
